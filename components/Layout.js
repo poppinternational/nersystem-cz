4de69f22480cfcb99755229ae7aecc40b050763b
Tomas Vestenicky <vestenicky@icloud.com>
2021-04-26 14:56:03 +0200

    Favicon

--- components/Layout.js
+++ components/Layout.js
@@ -81,6 +81,24 @@ const Layout = ({ children }) => {
       <Head>
         <title>nersystem</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <div className="min-h-screen">
         <div className="relative overflow-hidden">
