8e598689cb7f226f74287294a54bf96073355a72
Tomas Vestenicky <vestenicky@icloud.com>
2021-08-06 11:36:31 +0200

    App screenshot

--- pages/solutions/attendance.js
+++ pages/solutions/attendance.js
@@ -144,9 +144,9 @@ const Attendance = () => {
             <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
               <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
                 <img
-                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                  src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
-                  alt="Customer profile user interface"
+                  className="w-full max-w-xl mx-auto lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:h-full lg:w-auto lg:max-w-none"
+                  src="/app.png"
+                  alt="App"
                 />
               </div>
             </div>
