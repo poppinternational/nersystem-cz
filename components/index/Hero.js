2e80f49e6dbcbaac1266c7d7f0a547fe50caa8f8
Tomas Vestenicky <vestenicky@icloud.com>
2021-08-06 09:15:12 +0200

    Hero image and contact info

--- components/index/Hero.js
+++ components/index/Hero.js
@@ -84,8 +84,8 @@ const Hero = () => {
             <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
               <img
                 className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
-                src="/screenshot.png"
-                alt="Screenshot"
+                src="/hero.jpg"
+                alt=""
               />
             </div>
           </div>
