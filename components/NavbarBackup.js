3cfa885898b00c739d3e1e728e010cc7c80de6e8
Tomas Vestenicky <vestenicky@icloud.com>
2021-08-06 09:21:24 +0200

    Clean up menu

--- components/NavbarBackup.js
+++ components/NavbarBackup.js
@@ -56,6 +56,7 @@ const solutions = [
   },
 ];
 const callsToAction = [
+  { name: "Video prehliadka", href: "#", icon: PlayIcon },
   { name: "Kontaktujte nás", href: "/contact", icon: PhoneIcon },
 ];
 const resources = [
@@ -102,7 +103,7 @@ const Navbar = () => {
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center py-3 md:justify-start md:space-x-10">
-              <div className="flex justify-start">
+              <div className="flex justify-start lg:w-0 lg:flex-1">
                 <Link href="/">
                   <a>
                     <span className="sr-only">NER System</span>
@@ -199,9 +200,115 @@ const Navbar = () => {
                       </>
                     )}
                   </Popover>
+
+                  <a
+                    href="#"
+                    className="text-base font-medium text-gray-500 hover:text-gray-900"
+                  >
+                    O nás
+                  </a>
+
+                  <Popover className="relative">
+                    {({ open }) => (
+                      <>
+                        <Popover.Button
+                          className={classNames(
+                            open ? "text-gray-900" : "text-gray-500",
+                            "group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                          )}
+                        >
+                          <span>Viac</span>
+                          <ChevronDownIcon
+                            className={classNames(
+                              open ? "text-gray-600" : "text-gray-400",
+                              "ml-2 h-5 w-5 group-hover:text-gray-500"
+                            )}
+                            aria-hidden="true"
+                          />
+                        </Popover.Button>
+
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          enter="transition ease-out duration-200"
+                          enterFrom="opacity-0 translate-y-1"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="transition ease-in duration-150"
+                          leaveFrom="opacity-100 translate-y-0"
+                          leaveTo="opacity-0 translate-y-1"
+                        >
+                          <Popover.Panel
+                            static
+                            className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0"
+                          >
+                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                              <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                {resources.map((item) => (
+                                  <Link href={item.href} key={item.name}>
+                                    <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                                      <item.icon
+                                        className="flex-shrink-0 h-6 w-6 text-blue-600"
+                                        aria-hidden="true"
+                                      />
+                                      <div className="ml-4">
+                                        <p className="text-base font-medium text-gray-900">
+                                          {item.name}
+                                        </p>
+                                        <p className="mt-1 text-sm text-gray-500">
+                                          {item.description}
+                                        </p>
+                                      </div>
+                                    </a>
+                                  </Link>
+                                ))}
+                              </div>
+                              <div className="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
+                                <div>
+                                  <h3 className="text-sm tracking-wide font-medium text-gray-500 uppercase">
+                                    Najnovšie články
+                                  </h3>
+                                  <ul className="mt-4 space-y-4">
+                                    {recentPosts.map((post) => (
+                                      <li
+                                        key={post.id}
+                                        className="text-base truncate"
+                                      >
+                                        <Link href={post.href}>
+                                          <a className="font-medium text-gray-900 hover:text-gray-700">
+                                            {post.name}
+                                          </a>
+                                        </Link>
+                                      </li>
+                                    ))}
+                                  </ul>
+                                </div>
+                                <div className="mt-5 text-sm">
+                                  <Link href="#">
+                                    <a className="font-medium text-blue-600 hover:text-blue-500">
+                                      Všetky články
+                                      <span aria-hidden="true">
+                                        &nbsp;&rarr;
+                                      </span>
+                                    </a>
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
+                          </Popover.Panel>
+                        </Transition>
+                      </>
+                    )}
+                  </Popover>
                 </Popover.Group>
               )}
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                {MENU_SHOWN && (
+                  <Link href="#">
+                    <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                      Prihlásenie
+                    </a>
+                  </Link>
+                )}
                 <Link href="/contact">
                   <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
                     Kontaktujte nás
@@ -269,12 +376,39 @@ const Navbar = () => {
                   )}
                 </div>
                 <div className="py-6 px-5 space-y-6">
+                  {MENU_SHOWN && (
+                    <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                      <Link href="#">
+                        <a className="text-base font-medium text-gray-900 hover:text-gray-700">
+                          O nás
+                        </a>
+                      </Link>
+                      {resources.map((item) => (
+                        <Link key={item.name} href={item.href}>
+                          <a className="text-base font-medium text-gray-900 hover:text-gray-700">
+                            {item.name}
+                          </a>
+                        </Link>
+                      ))}
+                    </div>
+                  )}
                   <div>
                     <Link href="/contact">
                       <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
                         Kontaktujte nás
                       </a>
                     </Link>
+                    {MENU_SHOWN && (
+                      <p className="mt-6 text-center text-base font-medium text-gray-500">
+                        Máte NER System účet?{" "}
+                        <a
+                          href="#"
+                          className="text-blue-600 hover:text-blue-500"
+                        >
+                          Prihlásenie
+                        </a>
+                      </p>
+                    )}
                   </div>
                 </div>
               </div>
